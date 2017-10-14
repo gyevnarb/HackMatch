f050b47429a97a1876f658599a7a7441c6665027
FireCube- <gyevnarb@gmail.com>
2017-10-14 07:52:33 +0200

    More bugfixing

--- HackMatch/Resources/Resource.Designer.cs
+++ HackMatch/Resources/Resource.Designer.cs
@@ -60,23 +60,26 @@ namespace HackMatch
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int authenticateButton = 2131034116;
-			
-			// aapt resource value: 0x7f050002
-			public const int editText1 = 2131034114;
+			// aapt resource value: 0x7f050005
+			public const int authenticateButton = 2131034117;
 			
 			// aapt resource value: 0x7f050003
-			public const int editText2 = 2131034115;
+			public const int editText1 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int editText2 = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int matchingViewButton = 2131034113;
 			
 			// aapt resource value: 0x7f050000
 			public const int profileViewButton = 2131034112;
 			
-			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			// aapt resource value: 0x7f050002
+			public const int textView1 = 2131034114;
 			
-			// aapt resource value: 0x7f050005
-			public const int textView2 = 2131034117;
+			// aapt resource value: 0x7f050006
+			public const int textView2 = 2131034118;
 			
 			static Id()
 			{
