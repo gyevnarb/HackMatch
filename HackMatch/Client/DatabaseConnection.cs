34cb8dc63495e79e7dea06e975d5c1a102d59032
Murto <murto@airmail.cc>
2017-10-14 03:27:23 +0200

    Add beginning of DatabaseConnection class.

---
+++ HackMatch/Client/DatabaseConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+
+public class DatabaseConnection : IDatabaseCommunicator
+{
+
+	private TcpClient connection;
+
+	public DatabaseConnection(String server, Int32 port)
+	{
+		connection = new TcpClient(server, port);
+	}
+
+	public void IDatabaseCommunicator.CreateProfile(User userdata)
+	{
+
+	}
+	
+}
