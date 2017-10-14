42eda96f66aae7ca868e5673c3b38bf088fedc67
Murto <murto@airmail.cc>
2017-10-14 02:51:54 +0200

    Add IDatabaseCommunicator interface.

---
+++ HackMatch/Server/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface IDatabaseCommunicator
+{
+	bool CreateProfile(User userdata);
+	bool EditProfile(User userdata);
+
+	User LoadProfile(String userid);
+	int CalculateScore(String userid1, String userid2);
+}
