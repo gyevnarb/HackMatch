a725bc4c867c8e16012befdb6c58bbb44d947634
FireCube- <gyevnarb@gmail.com>
2017-10-14 02:43:36 +0200

    Added client code

---
+++ HackMatch/Client/User.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace HackMatch
+{
+    public class User
+    {
+        /// <summary>
+        /// Property to get and set first names of user
+        /// </summary>
+        public List<string> FirstNames { get; set; }
+
+        /// <summary>
+        /// Property to get and set last names of user
+        /// </summary>
+        public List<string> LastNames { get; set; }
+
+        /// <summary>
+        /// Property to get and set the user biography
+        /// </summary>
+        public string Bio { get; set; }
+
+        /// <summary>
+        /// Property to get and set technologies (including programming languages) and their corresponding experience level
+        /// </summary>
+        public Dictionary<string, ExperienceLevel> Technologies { get; set; }
+
+        /// <summary>
+        /// Property to get and set spoken languages
+        /// </summary>
+        public List<string> SpokenLanguages { get; set; }
+    }
+}
