90b2e1372b3a5e16982b994ade2270d0dfdbf679
Murto <murto@airmail.cc>
2017-10-14 04:07:44 +0200

    Add interface functions.

--- HackMatch/Client/ServerConnection.cs
+++ HackMatch/Client/ServerConnection.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Sockets;
-using System.Runtime.Serialization;
 
 namespace HackMatch
 {
@@ -21,12 +20,48 @@ namespace HackMatch
 		}
 
 		/// <summary>
-		/// Implements the CreateProfile function as described in IServerCommunicator
+		/// Implements the CreateProfile function as described in IServerCommunicator.
 		/// </summary>
 		public void IServerCommunicator.CreateProfile(User userdata)
 		{
-		
+			//	Need serialization before writing this part.
+			byte[] data = "CREATE ";
+			NetworkStream create = connection.GetStream();
+			create.Write(data, 0, data.Length);
 		}
 
+		/// <summary>
+		/// Implements the EditProfile function as described in IServerCommunicator.
+		/// </summary>
+		public void IServerCommunicator.EditProfile(User userdata)
+		{
+			//	Need serialization before writing this part.
+			byte[] data = "EDIT ";
+			NetworkStream edit = connection.GetStream();
+			edit.Write(data, 0, data.Length);
+		}
+
+		/// <summary>
+		/// Implements the LoadProfile function as described in IServerCommunicator.
+		/// </summary>
+		public User IServerCommunicator.LoadProfile(string userid)
+		{
+			byte[] data = "LOAD " + userid;
+			NetworkStream load = connection.GetStream();
+			load.Write(data, 0, data.Length);
+
+			//	Deserialize from load stream here.
+			//	return profile;
+		}
+
+		/// <summary>
+		/// Implements the CalculateScore function as described in IServerCommunicator.
+		/// </summary>
+		public int IServerCommunicator.CalculateScore(string userid1, string userid2)
+		{
+			byte[] data = "SCORE " + userid1 + ' ' + userid2;
+			NetworkStream score = connection.GetStream();
+			score.Write(data);
+		}
 	}
 }
