f15ff4967646ba18e656b2ff146887df1e5d6a91
Gwion ap Rheinallt <uoetranshumanistsociety@gmail.com>
2017-10-14 04:18:19 +0200

    Made a basic login UI for testing

--- HackMatch/MainActivity.cs
+++ HackMatch/MainActivity.cs
@@ -12,7 +12,15 @@ namespace HackMatch
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+             SetContentView (Resource.Layout.Main);
+            EditText editText1 = FindViewById<EditText>(Resource.Id.editText1);
+            TextView textView2 = FindViewById<EditText>(Resource.Id.textView2);
+            editText1.Text = "Yay, edit works!";
+            editText1.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {
+
+                textView2.Text = e.Text.ToString();
+            
+            };
         }
     }
 }
