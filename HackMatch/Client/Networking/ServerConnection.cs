8d938bc3fc8b0ed33f270b53dcad99bd69fb5cef
Murto <murto@airmail.cc>
2017-10-14 07:52:20 +0200

    Yeeeeeeeaaaaaaahhhhh    bbbbbooooooiiiiiiii (Basic networking tests)

--- HackMatch/Client/Networking/ServerConnection.cs
+++ HackMatch/Client/Networking/ServerConnection.cs
@@ -11,22 +11,16 @@ namespace HackMatch
 	public class ServerConnection : IServerCommunicator
 	{
 
-		private TcpClient connection;
+		public string Server { get; set; }
+		public Int32 Port { get; set; }
 
 		/// <summary>
 		/// Creates a new connection to the given server on the given port.
 		/// </summary>
 		public ServerConnection(string server, Int32 port)
 		{
-			connection = new TcpClient(server, port);
-		}
-
-		/// <summary>
-		/// Closes the current connection.
-		/// </summary>
-		~ServerConnection()
-		{
-			connection.Close();
+			Server = server;
+			Port = port;
 		}
 
 		/// <summary>
@@ -34,11 +28,13 @@ namespace HackMatch
 		/// </summary>
 		void IServerCommunicator.CreateProfile(User userdata)
 		{
+			TcpClient connection = new TcpClient(Server, Port);
 			byte[] data = Encoding.UTF8.GetBytes("CREATE ");
 			NetworkStream create = connection.GetStream();
 			create.Write(data, 0, data.Length);
 			DataContractJsonSerializer json = new DataContractJsonSerializer(userdata.GetType());
 			json.WriteObject(create, userdata);
+			connection.Close();
 		}
 
 		/// <summary>
@@ -46,12 +42,14 @@ namespace HackMatch
 		/// </summary>
 		void IServerCommunicator.EditProfile(User userdata)
 		{
+			TcpClient connection = new TcpClient(Server, Port);
 			//	Need serialization before writing this part.
 			byte[] data = Encoding.UTF8.GetBytes("EDIT ");
 			NetworkStream edit = connection.GetStream();
 			edit.Write(data, 0, data.Length);
 			DataContractJsonSerializer json = new DataContractJsonSerializer(userdata.GetType());
 			json.WriteObject(edit, userdata);
+			connection.Close();
 		}
 
 		/// <summary>
@@ -59,10 +57,12 @@ namespace HackMatch
 		/// </summary>
 		User IServerCommunicator.LoadProfile(string userid)
 		{
+			TcpClient connection = new TcpClient(Server, Port);
 			byte[] data = Encoding.UTF8.GetBytes("LOAD " + userid);
 			NetworkStream load = connection.GetStream();
 			load.Write(data, 0, data.Length);
 			DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(User));
+			connection.Close();
 			return (User)json.ReadObject(load);
 		}
 
@@ -71,11 +71,13 @@ namespace HackMatch
 		/// </summary>
 		Int32 IServerCommunicator.CalculateScore(string userid1, string userid2)
 		{
+			TcpClient connection = new TcpClient(Server, Port);
 			byte[] data = Encoding.UTF8.GetBytes("SCORE " + userid1 + ' ' + userid2);
 			NetworkStream score = connection.GetStream();
 			score.Write(data, 0, data.Length);
 			byte[] result = new byte[4];
 			score.Read(result, 0, 4);
+			connection.Close();
 			return BitConverter.ToInt32(data, 0);
 		}
 	}
--- HackMatch/Client/Utilities/Constants.cs
+++ HackMatch/Client/Utilities/Constants.cs
@@ -1,7 +1,13 @@
-﻿namespace HackMatch
+﻿using System;
+
+namespace HackMatch
 {
     public static class Constants
     {
         public const string HACKATHONS_XML = "Hackathons.xml";
+
+		//	Placeholder values for testing purposes.
+		public const string SERVER = "127.0.0.1";
+		public const Int32 PORT = 6969;
     }
 }
