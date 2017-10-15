f9baab3234442bbcd77645c5f8f94d50a2c0000b
FireCube- <gyevnarb@gmail.com>
2017-10-15 02:41:20 +0200

    Minor fixes to design

--- HackMatch/Resources/Resource.Designer.cs
+++ HackMatch/Resources/Resource.Designer.cs
@@ -54,7 +54,10 @@ namespace HackMatch
 			public const int junction = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int me = 2130837507;
+			public const int LausannePano = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int me = 2130837508;
 			
 			static Drawable()
 			{
@@ -69,14 +72,14 @@ namespace HackMatch
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06000a
-			public const int authenticateButton = 2131099658;
+			// aapt resource value: 0x7f06000f
+			public const int authenticateButton = 2131099663;
 			
-			// aapt resource value: 0x7f06001c
-			public const int bioLayout = 2131099676;
+			// aapt resource value: 0x7f060020
+			public const int bioLayout = 2131099680;
 			
-			// aapt resource value: 0x7f06001f
-			public const int bioTextView = 2131099679;
+			// aapt resource value: 0x7f060023
+			public const int bioTextView = 2131099683;
 			
 			// aapt resource value: 0x7f060003
 			public const int contactsViewButton = 2131099651;
@@ -84,86 +87,98 @@ namespace HackMatch
 			// aapt resource value: 0x7f060002
 			public const int eventsViewButton = 2131099650;
 			
+			// aapt resource value: 0x7f060015
+			public const int fnameTextView = 2131099669;
+			
+			// aapt resource value: 0x7f060013
+			public const int groupLayout1 = 2131099667;
+			
 			// aapt resource value: 0x7f060011
-			public const int fnameTextView = 2131099665;
+			public const int groupLayout3 = 2131099665;
 			
-			// aapt resource value: 0x7f06000f
-			public const int groupLayout1 = 2131099663;
+			// aapt resource value: 0x7f060005
+			public const int imageButton1 = 2131099653;
 			
-			// aapt resource value: 0x7f06000d
-			public const int groupLayout3 = 2131099661;
+			// aapt resource value: 0x7f060007
+			public const int imageButton2 = 2131099655;
+			
+			// aapt resource value: 0x7f060009
+			public const int imageButton3 = 2131099657;
+			
+			// aapt resource value: 0x7f06000b
+			public const int imageButton4 = 2131099659;
 			
 			// aapt resource value: 0x7f060004
 			public const int linearLayout1 = 2131099652;
 			
-			// aapt resource value: 0x7f060016
-			public const int linearLayout3 = 2131099670;
+			// aapt resource value: 0x7f06001a
+			public const int linearLayout3 = 2131099674;
 			
-			// aapt resource value: 0x7f060019
-			public const int linearLayout4 = 2131099673;
-			
-			// aapt resource value: 0x7f06000e
-			public const int linearLayout5 = 2131099662;
+			// aapt resource value: 0x7f06001d
+			public const int linearLayout4 = 2131099677;
 			
 			// aapt resource value: 0x7f060012
-			public const int lnameTextView = 2131099666;
+			public const int linearLayout5 = 2131099666;
+			
+			// aapt resource value: 0x7f060016
+			public const int lnameTextView = 2131099670;
 			
 			// aapt resource value: 0x7f060001
 			public const int matchingViewButton = 2131099649;
 			
-			// aapt resource value: 0x7f060014
-			public const int natLangTextView = 2131099668;
-			
-			// aapt resource value: 0x7f060009
-			public const int passWord = 2131099657;
-			
 			// aapt resource value: 0x7f060018
-			public const int profileImageView = 2131099672;
+			public const int natLangTextView = 2131099672;
 			
-			// aapt resource value: 0x7f060015
-			public const int profileLayout = 2131099669;
+			// aapt resource value: 0x7f06000e
+			public const int passWord = 2131099662;
 			
-			// aapt resource value: 0x7f060017
-			public const int profileTextView = 2131099671;
+			// aapt resource value: 0x7f06001c
+			public const int profileImageView = 2131099676;
+			
+			// aapt resource value: 0x7f060019
+			public const int profileLayout = 2131099673;
+			
+			// aapt resource value: 0x7f06001b
+			public const int profileTextView = 2131099675;
 			
 			// aapt resource value: 0x7f060000
 			public const int profileViewButton = 2131099648;
 			
-			// aapt resource value: 0x7f06001e
-			public const int scrollView1 = 2131099678;
-			
-			// aapt resource value: 0x7f06000c
-			public const int signUpButton = 2131099660;
-			
-			// aapt resource value: 0x7f06001b
-			public const int techTextView = 2131099675;
-			
-			// aapt resource value: 0x7f060005
-			public const int textView1 = 2131099653;
-			
-			// aapt resource value: 0x7f060006
-			public const int textView2 = 2131099654;
-			
-			// aapt resource value: 0x7f060008
-			public const int textView3 = 2131099656;
-			
-			// aapt resource value: 0x7f06000b
-			public const int textView4 = 2131099659;
-			
-			// aapt resource value: 0x7f060013
-			public const int textView5 = 2131099667;
-			
-			// aapt resource value: 0x7f06001a
-			public const int textView6 = 2131099674;
-			
-			// aapt resource value: 0x7f06001d
-			public const int textView7 = 2131099677;
+			// aapt resource value: 0x7f060022
+			public const int scrollView1 = 2131099682;
 			
 			// aapt resource value: 0x7f060010
-			public const int unameTextView = 2131099664;
+			public const int signUpButton = 2131099664;
 			
-			// aapt resource value: 0x7f060007
-			public const int userName = 2131099655;
+			// aapt resource value: 0x7f06001f
+			public const int techTextView = 2131099679;
+			
+			// aapt resource value: 0x7f060006
+			public const int textView1 = 2131099654;
+			
+			// aapt resource value: 0x7f060008
+			public const int textView2 = 2131099656;
+			
+			// aapt resource value: 0x7f06000a
+			public const int textView3 = 2131099658;
+			
+			// aapt resource value: 0x7f06000c
+			public const int textView4 = 2131099660;
+			
+			// aapt resource value: 0x7f060017
+			public const int textView5 = 2131099671;
+			
+			// aapt resource value: 0x7f06001e
+			public const int textView6 = 2131099678;
+			
+			// aapt resource value: 0x7f060021
+			public const int textView7 = 2131099681;
+			
+			// aapt resource value: 0x7f060014
+			public const int unameTextView = 2131099668;
+			
+			// aapt resource value: 0x7f06000d
+			public const int userName = 2131099661;
 			
 			static Id()
 			{
